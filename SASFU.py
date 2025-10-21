b05ac771b8165f5d17ada0da4e424e6bbcf238fd
GabrielGL11 <gabrielguaman1105@gmail.com>
2025-10-20 22:30:07 -0500

    Actualizacion 2

--- SASFU.py
+++ SASFU.py
@@ -1,23 +1,23 @@
 '''Proyecto sobre el sistema de gestion de cupos universitarios'''
 import abc
 class Usuario(metaclass=abc.ABCMeta):#Clase abstracta Usuario
-    def __init__(self, cedula_pasaporte, nombre, apellido, correo):
+    def __init__(self, cedula_pasaporte, nombre, apellido, correo):#Atributos de la clase Usuario, que seran heredados por las clases hijas
         self.cedula_pasaporte = cedula_pasaporte
         self.nombre = nombre
         self.apellido = apellido
         self.correo = correo
-    @abc.abstractmethod
+    @abc.abstractmethod#Metodo abstracto iniciar_sesion
     def iniciar_sesion(self):
         pass
-    @abc.abstractmethod
+    @abc.abstractmethod#Metodo abstracto cerrar_sesion
     def cerrar_sesion(self):
         pass
-    def cargar_datos(self):
+    def cargar_datos(self):#Metodo cargar_datos
         pass
 
 class Administrador(Usuario):#Clase Hija Administrador de Usuario
-    def __init__(self, cedula_pasaporte, nombre, apellido, correo, cargo):
-        super().__init__(cedula_pasaporte, nombre, apellido, correo)
+    def __init__(self, cedula_pasaporte, nombre, apellido, correo, cargo)
+        super().__init__(cedula_pasaporte, nombre, apellido, correo)#Atributos heredados de Usuario más el atributo cargo
         self.cargo = cargo
     def iniciar_sesion(self):
         return f"Bienvenido de vuelta, Administrador {self.nombre}."
@@ -26,7 +26,7 @@ class Administrador(Usuario):#Clase Hija Administrador de Usuario
     
 class Aspirante(Usuario):#Clase Hija Aspirante de Usuario
     def __init__(self, cedula_pasaporte, nombre, apellido, correo, telefono, titulo):
-        super().__init__(cedula_pasaporte, nombre, apellido, correo)
+        super().__init__(cedula_pasaporte, nombre, apellido, correo)#Atributos heredados de Usuario más los atributos telefono y titulo
         self.telefono = telefono
         self.titulo = titulo
     def iniciar_sesion(self):
@@ -36,7 +36,7 @@ class Aspirante(Usuario):#Clase Hija Aspirante de Usuario
 
 class Soporte(Usuario):#Clase Hija Soporte de Usuario
     def __init__(self, cedula_pasaporte, nombre, apellido, correo, telefono):
-        super().__init__(cedula_pasaporte, nombre, apellido, correo)
+        super().__init__(cedula_pasaporte, nombre, apellido, correo)#Atributos heredados de Usuario más el atributo telefono
         self.telefono = telefono
     def iniciar_sesion(self):
         return f"Soporte {self.nombre} ha iniciado sesión."
@@ -45,7 +45,7 @@ class Soporte(Usuario):#Clase Hija Soporte de Usuario
     
 class Profesor(Usuario):#Clase Hija Profesor de Usuario
     def __init__(self, cedula_pasaporte, nombre, apellido, correo, facultad):
-        super().__init__(cedula_pasaporte, nombre, apellido, correo)
+        super().__init__(cedula_pasaporte, nombre, apellido, correo)#Atributos heredados de Usuario más el atributo facultad
         self.facultad = facultad
     def iniciar_sesion(self):
         return f"Profesor {self.nombre} ha iniciado sesión."
@@ -67,9 +67,13 @@ class Oferta_Academica:#Clase Oferta_Academica
         self.cantidad = cantidad
         
 class Periodo:#Clase Periodo
-    def __init__(self, Año_Lectivo, Semestre):
+    def __init__(self, Año_Lectivo, Semestre):#Atributos de la clase Periodo
         self.Año_Lectivo = Año_Lectivo
         self.Semestre = Semestre
+    def iniciar_periodo(self):#Metodo iniciar_periodo
+        return f"El periodo {self.Año_Lectivo} - {self.Semestre} ha iniciado."
+    def finalizar_periodo(self):#Metodo finalizar_periodo
+        return f"El periodo {self.Año_Lectivo} - {self.Semestre} ha finalizado."
 
 class Inscripcion:#Clase Inscripcion
     def __init__(self, carrera, facultad):
