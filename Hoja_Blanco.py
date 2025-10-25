da399de665ad374c74baaa34e3f3d1afedb0b827
GabrielGL11 <gabrielguaman1105@gmail.com>
2025-10-25 09:54:18 -0500

    Actualizacion 10

---
+++ Hoja_Blanco.py
@@ -0,0 +1,6 @@
+import SASFU
+class1=SASFU.Aspirante("1312685570","Juan","Perez","JuaPere@gmail.com","090900933",True,9.02)
+class1.cargar_datos()
+class1.iniciar_sesion()
+print(class1.nacionalidad())
+class1.cerrar_sesion()
