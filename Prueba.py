f67466b0f686338c547c1cfd5122478c728ea84e
GabrielGL11 <gabrielguaman1105@gmail.com>
2025-10-27 11:05:24 -0500

    Update 27

--- Prueba.py
+++ Prueba.py
@@ -1,8 +1,24 @@
 import SASFU
-aspirante1=SASFU.Aspirante("1312685560","Juan","Perez","JuaPere@gmail.com","090900933",True,9.02)
-aspirante1.cargar_datos()
+aspirante1 = SASFU.Aspirante("1312685560", "Juan", "Pérez", "juan@gmail.com", "099000933", True, 8.02)
+soport1 = SASFU.Soporte("0923456789", "María", "Gómez", "maria@soporte.com", "098765432")
+profesor1 = SASFU.Profesor("1002003004","Carlos","Lopez","carlez@hotmail.com", "arquitecto")
+aspirante2=SASFU.Aspirante("1312685560","Daniel","Correa","Daniel@gmail.com","097859433",True,8.02)
+aspirante2.cargar_datos()
+aspirante2.iniciar_sesion()
+print(aspirante2.nacionalidad())
+aspirante2.cerrar_sesion()
+profesor1.crear_cuestionario("Matemáticas", 10)
+
 aspirante1.iniciar_sesion()
-print(aspirante1.nacionalidad())
-aspirante1.cerrar_sesion()
-def test_nota_grado():
-    assert aspirante1.nota_grado==9.02
+soport1.iniciar_sesion()
+
+soport1.recibir_asistencia(aspirante1)
+
+hora = input("hora del examen:")
+modalidad = input("presencial o virtual:")
+sede = input("sede:")
+
+evaluacion1 = SASFU.Evaluacion(SASFU.mixto(), hora, modalidad, sede)
+evaluacion1.aplicar_examen(respuestas_correctas=45, total_preguntas=50)
+
+print(evaluacion1.mostrar_resultado())
