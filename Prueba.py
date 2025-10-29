9f280805c36742a3baa261f3c235292b0e02ea06
GabrielGL11 <gabrielguaman1105@gmail.com>
2025-10-28 22:55:46 -0500

    Update 32

--- Prueba.py
+++ Prueba.py
@@ -1,16 +1,8 @@
-import SASFU
+iimport SASFU
 aspirante1 = SASFU.Aspirante("1312685560", "Juan", "Pérez", "juan@gmail.com", "099000933", True, 8.02)
 soport1 = SASFU.Soporte("0923456789", "María", "Gómez", "maria@soporte.com", "098765432")
-profesor1 = SASFU.Profesor("1002003004","Carlos","Lopez","carlez@hotmail.com", "arquitecto")
-aspirante2=SASFU.Aspirante("1312685560","Daniel","Correa","Daniel@gmail.com","097859433",True,8.02)
-aspirante2.cargar_datos()
-aspirante2.iniciar_sesion()
-print(aspirante2.nacionalidad())
-aspirante2.cerrar_sesion()
+profesor1=SASFU.Profesor("1002003004","Carlos","Lopez","carlez@hotmail.com", "arquitecto")
 profesor1.crear_cuestionario("Matemáticas", 10)
-
-aspirante1.iniciar_sesion()
-soport1.iniciar_sesion()
 
 soport1.recibir_asistencia(aspirante1)
 
@@ -20,5 +12,9 @@ sede = input("sede:")
 
 evaluacion1 = SASFU.Evaluacion(SASFU.mixto(), hora, modalidad, sede)
 evaluacion1.aplicar_examen(respuestas_correctas=45, total_preguntas=50)
-
 print(evaluacion1.mostrar_resultado())
+
+postulacion1 = SASFU.Postulacion("Ingeniería en Software", 850)
+postulacion1.iniciar()
+postulacion1.seleccionar_carrera()
+postulacion1.finalizar()
